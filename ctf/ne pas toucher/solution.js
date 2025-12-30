4a6bc332099d3a0c7fa5dee9dd7bb083037cd8d6
titouannezreg-collab <nezregtitouan@gmail.com>
2025-12-30 11:53:11 +0100

    Fix web5 flag value in solution.js

--- ctf/ne pas toucher/solution.js
+++ ctf/ne pas toucher/solution.js
@@ -8,7 +8,7 @@ const flags = {
     web2: "flag{W3bInv3st!gat0r}",
     web3: "flag{illusion_regarde_pas_ce_que_tu_vois}",
     web4: "flag{ADMIN_ACCESS_GRANTED}",
-    Web5: "flag{123456azerty}",
+    web5: "flag{123456789azerty}",
     
     stegano1: "flag{presquecaché}",
     textures: "flag{stegapause}",
@@ -38,5 +38,6 @@ function checkFlag(challenge) {
     }
 
 }
+
 
 
